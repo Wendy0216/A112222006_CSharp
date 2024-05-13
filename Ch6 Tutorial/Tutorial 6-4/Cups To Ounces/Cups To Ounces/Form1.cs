2b8148d04315e272b470555b3bda54cbb2c7cf2a
Wendy0216 <a112222006@mail.shu.edu.tw>
2024-05-13 16:56:47 +0800

    113-05-13

--- Ch6 Tutorial/Tutorial 6-4/Cups To Ounces/Cups To Ounces/Form1.cs
+++ Ch6 Tutorial/Tutorial 6-4/Cups To Ounces/Cups To Ounces/Form1.cs
@@ -20,11 +20,27 @@ namespace Cups_To_Ounces
         // The CupsToOunces method accepts a number
         // of cups as an argument and returns the
         // equivalent number of fluid ounces.
+
+
+        private void CupsToOunces(double cups, out double ounces)
+        {
+            ounces =  cups * 8.0;
+        }
      
 
         private void convertButton_Click(object sender, EventArgs e)
         {
-          
+            double cups, ounces;
+
+            if(double.TryParse(cupsTextBox.Text, out cups))
+            {
+                CupsToOunces(cups, out ounces);
+                ouncesLabel.Text = ounces.ToString("nl");
+            }
+            else
+            {
+                MessageBox.Show("Enter a valid number.");
+            }
         }
 
         private void exitButton_Click(object sender, EventArgs e)
